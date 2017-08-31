3119dfbde01ae9b6b53b371519687cc0b6b2d694
Razvan <tangent.jotey@gmail.com>
2017-09-01 00:54:48 +0200

    Update Breadcumb.js

--- scripts/utils/Breadcumb.js
+++ scripts/utils/Breadcumb.js
@@ -1,69 +1,70 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 
-/*
- * 
- * <a href="#">aaaaa</a>
-    &gt;
-    <a href="#">bbbbb</a>
- * 
- * 
+/* BOOTSTRAP Breadcumb only
+ *
+ *
+ <ol id="main-breadcumb" class="breadcrumb">
+   <li><a href="#">AreaRiservata</a></li>
+   <li><a href="#">comunityAttive</a></li>
+   <li>[numero comuity]</li>
+ </ol>
+ *
+ *
  */
 
-function Breadcumb(target){
-    var 
-    $this = this,
-    selected,
-    items = new Array(),
-    object = (isset(target)?target:create("div"));
-    object.className="breadcumb";
-    this.push=function(name,uri){
-        var arrow = create("span");
-        arrow.className="arrow";
-        arrow.innerHTML=" > ";
-        var item = create("a");
-        item.className="item";
-        item.innerHTML=name;
-        item.onclick=function(){
-            go(uri);
-            if(this!=selected){
-                $this.pop();
-            }
-        };
-        items.push({
-            "arrow": arrow,
-            "item": item
-        });
-        if(object.innerHTML!="")
-            object.appendChild(arrow);
-        object.appendChild(item);
-        this.refresh();
-    };
-    this.pop=function(){
-        items[items.length-1]["arrow"].remove();
-        items[items.length-1]["item"].remove();
-        items.splice(-1,1);
-        this.refresh();
-    };
-    
-    this.clear=function(){
-        object.innerHTML="";
-    };
-    
-    this.getBreadcumb=function(){
-        return object;
-    };
-    this.refresh=function(){
-        foreach(items,function(item,i){
-            item["item"].className="item";
-            if(i==items.length-1){
-                item["item"].className +=" selected";
-                selected = item["item"];
-            }
-        });
-    };
+function Breadcumb(){
+  var bread = create("ol");
+  bread.className="breadcumb";
+  var items = new Array();
+  var i = 0;
+  var $this = this;
+  this.push=function(label,uri){
+    item = create("li");
+    console.log("LABEL:"+label);
+    if(isset(uri)){
+      anchor = create("a");
+      anchor.innerHTML = label;
+      anchor.onclick=e=>{
+        setContent(uri,window["main-content"]);
+      };
+      item.appendChild(anchor);
+    }else{
+      item.innerHTML = label;
+    }
+    items[i] = item;
+    bread.appendChild(item);
+    i++;
+  };
+
+  this.pop=function(){
+    items[i-1].remove();
+    delete items[i-1];
+    i--;
+  }
+
+  this.getElement=function(){
+    return bread;
+  }
+
+  this.update=function(){
+    //clear bread
+    bread.innerHTML = "";
+    //prepare tmp string
+    var concatUri = "";
+    //run through keywords
+    foreach(getJobLocation().split("/"),function(label,i,isLast){
+      //push keyword and relative uri
+      concatUri += (i===0?"":"/")+label;
+      if(isLast){
+        $this.push(label.capitalize());
+      }else{
+        $this.push(label.capitalize(),concatUri);
+      }
+    });
+  }
 }
