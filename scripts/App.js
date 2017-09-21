9bdbad26f360f566591dab08396ea440a999c146
Razvan <tangent.jotey@gmail.com>
2017-09-21 23:01:47 +0200

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -2,4 +2,9 @@ window.App=function(){
     /*
     Layout definition requests should be sent from here
     */
+    if(getJobLocation() === ""){
+      setContent(Project.DEFAUL_CONTROLLER,window["main-content"],true);
+    }else{
+      setContent(getJobLocation(),window["main-content"],true);
+    }
 };
