b8d376b4a3cb6d56c266b82a8910126e01353042
Razvan <tangent.jotey@gmail.com>
2017-09-30 05:58:14 +0200

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,10 +1,10 @@
-Project.DEFAULT_CONTROLLER = "Home/main";
+Project.DEFAULT_CONTROLLER = "Home";
 Project.MTU = 32000;
 
-
+  window.vocabulary = new Vocabulary();
   window
     .vocabulary
-    .addPage(new Page("collabora"));
+    .addPage(new Page("general"));
 
   window
     .vocabulary
@@ -16,19 +16,19 @@ Project.MTU = 32000;
 /*
   HOW TO USE:
   given the tag h3...
-  
-  <h3 @="general/example"></h3> 
-  
+
+  <h3 @="general/example"></h3>
+
   the attribute "@" specifies the variable to use, so the content of the tag would become:
-  
+
   <h3 @="general/example">example</h3>
-  
+
   if localStorage.getItem("language") === "en"
-  
+
   or
-  
+
   <h3 @="general/example">esempio</h3>
-  
+
   if localStorage.getItem("language") === "it"
-  
+
 */
