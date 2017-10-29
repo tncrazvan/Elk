882de9c84055e8123273183984b66a5e0899f11e
Razvan <tangent.jotey@gmail.com>
2017-10-29 23:46:26 +0100

    Delete Rgba.js

--- scripts/Rgba.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function Rgba(red,green,blue,alfa){
-    return new String("rgba("+red+","+green+","+blue+","+alfa+")");
-}
