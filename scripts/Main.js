1498eb08721198107648f830efc866da79502d12
Razvan <tangent.jotey@gmail.com>
2017-10-30 01:21:55 +0100

    Update Main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -517,7 +517,15 @@ function go(link, onready, target) {
 
 //basically does the same thing as go(ling, onready, target), but it's more straight forward
 function setContent(uri,target,changeState,allowVariables){
-  new HttpEvent("/@"+uri,function(result){
+  uri = uri.trim();
+  let uri_prepend = "";
+  if(uri.substr(0,Project.URI_FILE_DELIMITER.length) === Project.URI_FILE_DELIMITER){
+    uri_prepend = "";
+    uri = uri.substr(Project.URI_FILE_DELIMITER.length);
+  }else{
+    uri_prepend = "/@";
+  }
+  new HttpEvent(uri_prepend+uri,function(result){
     if(isset(changeState))
       if(changeState){
         history.pushState(null, document.title, Project.workspace + '/' + uri);
@@ -1040,55 +1048,55 @@ function Rgba(red,green,blue,alfa){
 }
 
 function Popup(url,title,i) {
-    
+
     if(isset(i.toolbar)){
     	if(i.toolbar) i.toolbar = 'yes';
     	if(!i.toolbar) i.toolbar = 'no';
     }else i.toolbar = 'no';
-    
+
     if(isset(i.location)){
     	if(i.location) i.location = 'yes';
     	if(!i.location) i.location = 'no';
     }else i.location = 'no';
-    
+
     if(isset(i.directories)){
     	if(i.directories) i.directories = 'yes';
     	if(!i.directories) i.directories = 'no';
     }else i.directories = 'no';
-    
+
     if(isset(i.status)){
     	if(i.status) i.status = 'yes';
     	if(!i.status) i.status = 'no';
     }else i.status = 'no';
-    
+
     if(isset(i.menubar)){
     	if(i.menubar) i.menubar = 'yes';
     	if(!i.menubar) i.menubar = 'no';
     }else i.menubar = 'no';
-    
+
     if(isset(i.scrollbars)){
     	if(i.scrollbars) i.scrollbars = 'yes';
     	if(!i.scrollbars) i.scrollbars = 'no';
     }else i.scrollbars = 'no';
-    
+
     if(isset(i.resizable)){
     	if(i.resizable) i.resizable = 'yes';
     	if(!i.resizable) i.resizable = 'no';
     }else i.resizable = 'no';
-    
+
     if(isset(i.copyhistory)){
     	if(i.copyhistory) i.copyhistory = 'yes';
     	if(!i.copyhistory) i.copyhistory = 'no';
     }else i.copyhistory = 'no';
-    
+
     if(!isset(i.width)) i.width = 800;
     if(!isset(i.height)) i.height = 500;
-    
+
 
     i.left = (screen.width/2)-(i.width/2);
     i.top = (screen.height/2)-(i.height/2);
-    
-    return window.open(url, title, 
+
+    return window.open(url, title,
     		'toolbar='+i.toolbar+', '+
     		'location='+i.location+', '+
     		'directories='+i.directories+', '+
@@ -1101,4 +1109,4 @@ function Popup(url,title,i) {
     		'height='+i.height+', '+
     		'top='+i.top+', '+
     		'left='+i.left+'');
-} 
+}
