8ba7e148317fa10e075c41df61cb497085fc23fe
Razvan <tangent.jotey@gmail.com>
2017-11-30 21:29:49 +0100

    includes now return a promise when they are done

--- scripts/framework/include.js
+++ scripts/framework/include.js
@@ -4,65 +4,102 @@ Project.ready = false;
 function Project(){}
 window.workspace = Project.workspace;
 
+window.modules = document.createElement("div");
+window.modules.setAttribute("id","modules");
+window.modules.style.display = "none";
+
 function include(){}
 
+include.modules = function(list){
+  if(list.constructor !== Array){
+    return include.module([list]);
+  }else{
+    return new Promise(function(resolve,reject){
+      let i = 0, length = list.length;
+      if(length>0){
+        (function poll(){
+          i++;
+          let file = list[i-1]; //without extension
+          new HttpEvent("modules/"+file+".html",function(result){
+            let moduleHolder = document.createElement("module");
+            moduleHolder.applyHtml(result);
+            (resolve)();
+            if(i<length){
+              poll();
+            }
+          }).run();
+        })();
+      }
+    });
+  }
+};
+include.module = function(list){
+  return include.modules(list);
+};
+
 include.css = function(list){
-    if(list.constructor !== Array){
-        include.css([list]);
-    }else{
-        var i = 0, length = list.length;
-        if(length>0){
-            (function poll(){
-                i++;
-                var file = list[i-1]; //without extension
-                var style = document.createElement("link");
-                    style.setAttribute("rel","stylesheet");
-                    style.setAttribute("type","text/css");
-                    if(file.charAt(0)==="@"){
-                        style.setAttribute("href",(file.replace("@","")));
-                    }else{
-                        style.setAttribute("href",workspace+"assets/css/"+(file/*.replace(".","/")*/)+".css");
-                    }
-                    document.head.appendChild(style);
-                    style.onload=function(){
-                        if(i<length){
-                            poll();
-                        }
-                    };
-            })();
-        }
-    }
+  if(list.constructor !== Array){
+    return include.css([list]);
+  }else{
+    return new Promise(function(resolve,reject){
+      let i = 0, length = list.length;
+      if(length>0){
+        (function poll(){
+          i++;
+          let file = list[i-1]; //without extension
+          let style = document.createElement("link");
+          style.setAttribute("rel","stylesheet");
+          style.setAttribute("type","text/css");
+          if(file.charAt(0)==="@"){
+            style.setAttribute("href",(file.replace("@","")));
+          }else{
+            style.setAttribute("href",workspace+"assets/css/"+file+".css");
+          }
+          document.head.appendChild(style);
+          style.onload=function(){
+            (resolve)();
+            if(i<length){
+              poll();
+            }
+          };
+        })();
+      }
+    });
+  }
 };
 include.js = function(list){
-    if(list.constructor !== Array){
-        include.js([list]);
-    }else{
-        var i = 0, length = list.length;
-        if(list.constructor === Array && length>0){
-            (function poll(){
-                i++;
+  if(list.constructor !== Array){
+    return include.js([list]);
+  }else{
+    return new Promise(function(resolve,reject){
+      let i = 0, length = list.length;
+      if(list.constructor === Array && length>0){
+        (function poll(){
+          i++;
 
-                var file = list[i-1]; //without extension
-                var script = document.createElement("script");
-                    script.setAttribute("type","text/javascript");
-                    script.setAttribute("charset","UTF-8");
-                    if(file.charAt(0)==="@"){
-                        script.setAttribute("src",(file.replace("@","")));
-                    }else{
-                        script.setAttribute("src",workspace+"scripts/"+(file)+".js");
-                    }
-                    document.body.appendChild(script);
-                    script.onload=function(){
-                        if(i<length){
-                            poll();
-                        }else{
-                            if(MainActivity.counter===0){
-                                Project.ready = true;
-                                new MainActivity();
-                            }
-                        }
-                    };
-            })();
-        }
-    }
+          let file = list[i-1]; //without extension
+          let script = document.createElement("script");
+          script.setAttribute("type","text/javascript");
+          script.setAttribute("charset","UTF-8");
+          if(file.charAt(0)==="@"){
+            script.setAttribute("src",(file.replace("@","")));
+          }else{
+            script.setAttribute("src",workspace+"scripts/"+file+".js");
+          }
+          document.body.appendChild(script);
+          script.onload=function(){
+            (resolve)();
+            if(i<length){
+                poll();
+            }else{
+                if(MainActivity.counter===0){
+                    Project.ready = true;
+                    new MainActivity();
+                }
+            }
+          };
+        })();
+      }
+    });
+  }
 };
