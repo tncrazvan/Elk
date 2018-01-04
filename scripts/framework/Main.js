fb0751767565d5af4925973c0550490496000cb4
Razvan <tangent.jotey@gmail.com>
2018-01-04 16:16:16 +0100

    Update Main.js

--- scripts/framework/Main.js
+++ scripts/framework/Main.js
@@ -75,10 +75,30 @@ function isFunction(functionToCheck) {
  return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 }
 
+function isElement(obj) {
+  try {
+    //Using W3 DOM2 (works for FF, Opera and Chrome)
+    return obj instanceof HTMLElement;
+  }
+  catch(e){
+    //Browsers not supporting W3 DOM2 don't have HTMLElement and
+    //an exception is thrown and we end up here. Testing some
+    //properties that all elements have (works on IE7)
+    return (typeof obj==="object") &&
+      (obj.nodeType===1) && (typeof obj.style === "object") &&
+      (typeof obj.ownerDocument ==="object");
+  }
+}
+
 function create(tag,content,allowVariables){
     var element = document.createElement(tag);
     if(isset(content)){
-      element.applyHtml(content,allowVariables);
+      if(isElement(content)){
+        element.innerHTML = "";
+        element.appendChild(content);
+      }else{
+        element.applyHtml(content,allowVariables);
+      }
     }
     return element;
 }
