ff64b70ae6a1890584f06d4210cdfcc6ca2eac7f
Razvan <tangent.jotey@gmail.com>
2018-01-13 01:45:28 +0100

    Update VoiceGroupWorker.js

--- scripts/framework/VoiceGroupWorker.js
+++ scripts/framework/VoiceGroupWorker.js
@@ -1,9 +1,10 @@
 let ws = null;
-
+const CONNECTED = 0, DISCONNECTED = 1;
 self.onmessage=function(e){
   if(e.data.connect){
     ws = new WebSocket(e.data.connect);
     ws.onopen=function(e){
+      postMessage({status: CONNECTED});
       console.log("Connected to VoiceGroup");
     };
     ws.onmessage=function(e){
@@ -11,11 +12,10 @@ self.onmessage=function(e){
       postMessage(e.data);
     };
     ws.onclose=function(e){
-      postMessage({
-        disconnect: true
-      });
+      postMessage({status: DISCONNECTED});
       console.log("Disconnected from VoiceGroup");
     };
+
   }else if(e.data.disconnect){
     ws.close();
   }else {
