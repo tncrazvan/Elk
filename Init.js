edeaed8698ed0bb5be17ad9a12c3ecd4aed573ca
Razvan <tangent.jotey@gmail.com>
2018-01-16 14:39:54 +0100

    syntax error fixed

--- Init.js
+++ Init.js
@@ -10,7 +10,7 @@ loader.js([
   "Elk/App",
   "Elk//Index"
 ]).then(function(){
-    window.modules = create("div"):
+    window.modules = create("div");
     window.modules.style.display="none";
     document.body.appendChild(window.modules);
     Project.ready = true;
