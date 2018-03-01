68e0caefcbb3fcf53a0e19300ef01328f33881a1
Razvan <tangent.jotey@gmail.com>
2018-03-01 22:27:57 +0100

    include.js fixed

--- include.js
+++ include.js
@@ -55,6 +55,7 @@ function Includer(dir){
 function include(){}
 window.modules = document.createElement("div");
 window.modules.setAttribute("id","modules");
+window.modules.style.display="none";
 document.body.appendChild(window.modules);
 include.modules = function(dir,list,f){
   if(dir === "") dir = "/modules/";
