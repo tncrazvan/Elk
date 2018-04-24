7576d63b357da8eb4297beea7abe08699226ee94
Razvan <tangent.jotey@gmail.com>
2018-04-24 23:18:47 +0200

    swipe.setLeftMenu added

--- swipe.js
+++ swipe.js
@@ -54,3 +54,67 @@ function handleTouchMove(evt) {
     xDown = null;
     yDown = null;
 };
+
+
+swipe.setLeftMenu=function(menu){
+    swipe.start=(x,y)=>{
+        swipe.start.x = x;
+        swipe.start.y = y;
+    };
+    let dxDelta = 0;
+    swipe.moving=(x,y)=>{
+        if(!swipe.allow) return;
+
+        let dx = (x - swipe.start.x)/20 + menu.dxDelta;
+        if(dx > 0){
+            if(menu.state === 0 && swipe.start.x > menu.offsetWidth*.5){
+                return;
+            }
+        }else if(dx < 0){
+            if(menu.state === 1 && swipe.start.x > menu.offsetWidth*1.5){
+                return;
+            }
+        }
+
+
+        x = menu.offsetLeft+dx;
+        menu.style.zIndex = 4;
+        if(x >= 0){
+            x = 0;
+            menu.state = 1;
+            menu.dxDelta += -dx;
+        }else if(x <= -menu.offsetWidth){
+            x = -menu.offsetWidth;
+            menu.style.zIndex = 0;
+            menu.state = 0;
+            menu.dxDelta += -dx;
+        }
+
+        if(dx < 0){
+            if(x <= -menu.offsetWidth*.1){
+                menu.state = 0;
+            }
+        }else if(dx > 0){
+            if(x > -menu.offsetWidth*.9){
+                menu.state = 1;
+            }
+        }
+
+
+        menu.style.left = Pixel(x);
+    };
+
+    swipe.end=function(){
+        menu.dxDelta = 0;
+        if(menu.state === 1){
+            console.log("menu showing");
+            showMenu();
+        }else if(menu.state === 0){
+            console.log("menu is hidden");
+            hideMenu();
+        }
+    };
+
+    document.addEventListener('touchend', swipe.end, false);
+    document.addEventListener('touchcancel', swipe.end, false);
+};
